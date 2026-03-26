eeb65ee5ad772d896674d0700e913adaff1c1c45
AleZxZ <jhosep1999pb@gmail.com>
2026-03-26 15:33:13 -0400

    primer commit

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => res.send('Backend funcionando'));
-
-app.listen(3000, () => console.log('Servidor en puerto 3000'));
