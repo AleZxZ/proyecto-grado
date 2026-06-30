9037c364c887b0f278a5d01960e7399db81978f8
AleZxZ <jhosep1999pb@gmail.com>
2026-06-30 02:58:13 -0400

    actualizacion

--- backend/config/email.js
+++ backend/config/email.js
@@ -3,14 +3,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.MAILTRAP_HOST,
   port: Number(process.env.MAILTRAP_PORT),
+  secure: false,
   auth: {
     user: process.env.MAILTRAP_USER,
     pass: process.env.MAILTRAP_PASS,
-  }
+  },
+  connectionTimeout: 10000,
 });
 
 module.exports = transporter;
-
 
 
 
