fa809833ada2ae62ca8dc8fe902886d6f3caa6b6
AleZxZ <jhosep1999pb@gmail.com>
2026-06-30 02:21:16 -0400

    cambios de mailtrap

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,6 +1,21 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  }
+});
+
+module.exports = transporter;
+
+
+/*
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
   host: process.env.MAILTRAP_HOST,
   port: Number(process.env.MAILTRAP_PORT),
   auth: {
@@ -10,3 +25,4 @@ const transporter = nodemailer.createTransport({
 });
 
 module.exports = transporter;
+*/
